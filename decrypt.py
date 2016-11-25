d2d49f0c6ef82ef6dc632252d02a50785bd449ef
Curtis Hughey <hugheyc@uchicago.edu>
2016-11-25 17:45:24 -0500

    Some basic chi stuff, now testing

--- decrypt.py
+++ decrypt.py
@@ -6,4 +6,4 @@ import sys
 
 
 if __name__ == '__main__':
-	print(str(sys.argv))
+    print(str(sys.argv))
--- utils.py
+++ utils.py
@@ -1,7 +1,64 @@
 # General utilities used by multiple encryption algorithms
 
+from collections import defaultdict
 
+# text must be lower case!
 def rot(text, shift):
-	print(shift)
+    shiftedText = ""
 
+    lowera = ord('a')
+
+    for c in text:	
+        shiftedC = chr(((ord(c)-lowera+shift) % 26)+lowera)
+        shiftedText += shiftedC
+
+    return shiftedText
+
+def isAlpha(c):
+    return ord('a') <= ord(c) <= ord('z')
+
+def calcFreqs(text):
+    counts = defaultdict(int)
+
+    total = 0
+    for c in text:
+        if isAlpha(c):
+            counts[c] += 1
+            total += 1
+
+    freqs = defaultdict(int)
+
+    for c in counts.keys():
+        freqs[c] = counts[c] / total
+
+    return freqs
+
+
+def calcChiSquared(text, expectedFreqs):
+    actualFreqs = calcFreqs(text)
+
+    chiSquares = 0.0
+
+    lowera = ord('a')
+    for i in range(0, 25)
+        c = chr(lowera+i)
+        output += ((expectedFreqs[c]-actualFreqs[c])**2) / 2
+
+    return output
+
+def readExpectedFreqs(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+
+        freqs = dict()
+
+        for line in lines:
+            split = line.split()
+            c = split[0]
+            freq = float(split[1])
+            freqs[c] = freq
+
+        print(freqs)
+
+        return freqs
 
